ee4cf7673aeb439a4011a3241e6dcd10261eb6cc
vinaykumar2004vinay <vinaykumardasari2004@gmail.com>
2026-07-07 17:01:42 +0530

    JAVA

---
+++ Data Types/one.java
@@ -0,0 +1,8 @@
+public class one {
+    public static void main(String[] args) {
+        float a = 2.5f;
+        float b = 3.2f;
+        float r = a+b;
+        System.out.println(r);
+    }
+}
---
+++ Data Types/two.java
@@ -0,0 +1,28 @@
+public class two {
+    public static void main(String[] args) {
+        int i = 2;
+        byte by = 123;
+        short sh = 456;
+        long l = 852014564;
+
+        float f = 4.5f;
+        double d = 4.3;
+
+        char c = 'k';
+
+        boolean b = true;
+
+        System.out.println(i);
+        System.out.println(by);
+        System.out.println(sh);
+        System.out.println(l);
+
+        System.out.println(f);
+        System.out.println(d);
+
+        System.out.println(c);
+
+        System.out.println(b);
+
+    }
+}
