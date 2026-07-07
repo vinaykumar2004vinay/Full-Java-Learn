879edf481d2c704e468b724396a639bca07c5fdd
vinaykumar2004vinay <vinaykumardasari2004@gmail.com>
2026-07-07 15:51:42 +0530

    JAVA

---
+++ Variables/one.java
@@ -0,0 +1,10 @@
+class one
+{
+    public static void main(String args[])
+    {
+        int num1 = 5;
+        int num2 = 10;
+        int result = num1 +num2;
+        System.out.println(result);
+    }
+}
