ce53a55a1e4b0040f2b7e539f4cbb9eb81c2ef3a
vinaykumar2004vinay <vinaykumardasari2004@gmail.com>
2026-07-08 11:05:34 +0530

    JAVA

---
+++ Logical operators/five.java
@@ -0,0 +1,12 @@
+class five
+{
+    public static void main(String args[])
+    {
+        int x = 10;
+        int y = 100;
+
+        boolean r = !(x<y);
+
+        System.out.println(r);
+    }
+}
---
+++ Logical operators/three.java
@@ -0,0 +1,15 @@
+class three
+{
+    public static void main(String args[])
+    {
+        int x = 10;
+        int y = 100;
+
+        double a = 10.5;
+        double b = 11.3;
+
+        boolean r = x<y || a>b;
+
+        System.out.println(r);
+    }
+}
