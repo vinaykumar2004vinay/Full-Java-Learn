3834cf4f193955183df2123cfa9a76726cf7470f
vinaykumar2004vinay <vinaykumardasari2004@gmail.com>
2026-07-08 14:22:18 +0530

    JAVA

---
+++ For Loop/two.java
@@ -0,0 +1,14 @@
+class two
+{
+    public static void main(String[] args)
+    {
+        for(int i=1;i<=7;i++)
+        {
+            System.out.println("Day " + i);
+            for(int j=1;j<=9;j++)
+            {
+                System.out.println((j+7) + "-" + (j+8));
+            }
+        }
+    }
+}
