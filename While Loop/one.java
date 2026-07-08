2d4977aa31d274887e7d01ca6687dd9bd9b9656b
vinaykumar2004vinay <vinaykumardasari2004@gmail.com>
2026-07-08 13:52:06 +0530

    JAVA

---
+++ While Loop/one.java
@@ -0,0 +1,13 @@
+class one
+{
+    public static void main(String[] args)
+    {
+        int i = 0;
+        while(i<=5)
+        {
+            System.out.println("Hii" + i);
+            i++;
+        }
+        System.out.println("Byee" + i);
+    }
+}
---
+++ While Loop/two.java
@@ -0,0 +1,20 @@
+class two
+{
+    public static void main(String[] args)
+    {
+        int i = 1;
+        while(i<=5)
+        {
+            System.out.println("Hii" + i);
+            i++;
+
+            int j=1;
+            while(j<=3)
+            {
+                System.out.println("Hello" + j);
+                j++;
+            }
+        }
+        System.out.println("Byee" + i);
+    }
+}
