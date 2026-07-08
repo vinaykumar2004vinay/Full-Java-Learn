a95adf8bbf9cec3e0318cfca36f1d4f282158c82
vinaykumar2004vinay <vinaykumardasari2004@gmail.com>
2026-07-08 14:07:34 +0530

    JAVA

---
+++ DoWhile Loop/one.java
@@ -0,0 +1,13 @@
+class one
+{
+    public static void main(String[] args)
+    {
+        int i = 21;
+        do
+        {
+            System.out.println("Hii");
+            i++;
+        }
+        while(i<=5);
+    }
+}
