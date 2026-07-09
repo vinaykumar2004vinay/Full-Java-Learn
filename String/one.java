dea3974af376e0c024248e667d3b3c63ff73e5d2
vinaykumar2004vinay <vinaykumardasari2004@gmail.com>
2026-07-09 09:31:28 +0530

    JAVA

---
+++ String/one.java
@@ -0,0 +1,8 @@
+class one
+{
+    public static void main(String[] args)
+    {
+        String obj = new String("Vinay");
+        System.out.println(obj);
+    }
+}
---
+++ String/two.java
@@ -0,0 +1,8 @@
+class two
+{
+    public static void main(String[] args)
+    {
+        String obj = new String("Vinay");
+        System.out.println("Hello " + obj);
+    }
+}
