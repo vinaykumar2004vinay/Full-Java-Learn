d1070d07b46b0bf0afb3cb73e2dab3a73b4437dc
vinaykumar2004vinay <vinaykumardasari2004@gmail.com>
2026-07-09 10:54:11 +0530

    JAVA

---
+++ String Buffer&Reader/one.java
@@ -0,0 +1,9 @@
+class one
+{
+    public static void main(String args[])
+    {
+        StringBuffer SB = new StringBuffer("Vinay");
+        SB.append(" Kumar");
+        System.out.println(SB);
+    }
+}
