3db273b777f4dcea915d7f6c646857f904431ec8
vinaykumar2004vinay <vinaykumardasari2004@gmail.com>
2026-07-09 11:17:06 +0530

    JAVA

---
+++ Static Method/one.java
@@ -0,0 +1,45 @@
+class Mobile
+{
+    String Brand;
+    int Price;
+    static String Name;
+
+    public void show()
+    {
+        System.out.println(Brand + " - " + Price + " - " + Name);
+    }
+
+    public static void show1()
+    {
+        System.out.println("In Static");
+    }
+}
+
+public class one
+{
+    public static void main(String[] args)
+    {
+        Mobile obj1 = new Mobile();
+        obj1.Brand = "Samsung";
+        obj1.Price = 1000;
+        Mobile.Name = "Smartphone";
+
+        Mobile obj2 = new Mobile();
+        obj2.Brand = "Apple";
+        obj2.Price = 15458;
+        Mobile.Name = "Smartphone";
+
+        Mobile obj3 = new Mobile();
+        obj3.Brand = "Vivo";
+        obj3.Price = 1235;
+        Mobile.Name = "Smartphone";
+
+        obj1.Name = "Mobile";
+
+        obj1.show();
+        obj2.show();
+        obj3.show();
+
+        Mobile.show1();
+    }
+}
