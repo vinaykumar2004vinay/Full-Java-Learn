1e418f57a970032beb84f18a26c83ea78c15e08f
vinaykumar2004vinay <vinaykumardasari2004@gmail.com>
2026-07-12 18:40:21 +0530

    JAVA

---
+++ Access Modifiers/one.java
@@ -0,0 +1,10 @@
+import others.*;
+
+public class one
+{
+    public static void main(String[] args)
+    {
+        A obj = new A();
+        System.out.println(obj.Marks);
+    }
+}
---
+++ Access Modifiers/others/A.java
@@ -0,0 +1,6 @@
+package others;
+
+public class A
+{
+    public int Marks = 25;
+}
