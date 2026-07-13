14708e31e1d485b814090245c9b00da7bad3091c
vinaykumar2004vinay <vinaykumardasari2004@gmail.com>
2026-07-13 13:19:04 +0530

    JAVA

---
+++ Exception/two.java
@@ -0,0 +1,20 @@
+class two
+{
+    public static void main(String args[])
+    {
+        int i = 0;
+        int j = 0;
+        try
+        {
+            j = 18/i;
+        }
+        catch(Exception e)
+        {
+            System.out.println("Something Went Wrong.....");
+        }
+
+        System.out.println(j);
+
+        System.out.println("Byee.....");
+    }
+}
